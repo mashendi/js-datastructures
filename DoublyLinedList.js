884532582a7402668942cba5df8e05836b34e510
Muhammed Shendi <muhammedshendi@gmail.com>
2021-12-01 08:42:44 +0200

    Added DoublyLinkedList

--- DoublyLinedList.js
+++ DoublyLinedList.js
@@ -2,10 +2,11 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     const newNode = new Node(value);
     this.head = newNode;
@@ -21,67 +22,70 @@ class LinkedList {
       this.tail = newNode;
     } else {
       this.tail.next = newNode;
+      newNode.prev = this.tail;
       this.tail = newNode;
     }
+
     this.length++;
 
     return this;
   }
 
   pop() {
-    // if list is empty
-    if (!this.head) {
+    if (this.length === 0) {
       return undefined;
     }
 
-    let temp = this.head;
-    let pre = this.head;
+    let temp = this.tail;
 
-    while (temp.next) {
-      pre = temp;
-      temp = temp.next;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
     }
 
-    this.tail = pre;
-    this.tail.next = null;
     this.length--;
-
-    if (this.length === 0) {
-      this.head = this.tail = null;
-    }
 
     return temp;
   }
 
   unshift(value) {
-    let newNode = new Node(value);
+    const newNode = new Node(value);
 
-    if (!this.head) {
+    if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
       newNode.next = this.head;
+      this.head.prev = newNode;
       this.head = newNode;
     }
 
     this.length++;
+
     return this;
   }
 
   shift() {
-    let temp = this.head;
-
-    if (!this.head) {
+    if (this.length === 0) {
       return undefined;
     }
 
-    this.head = this.head.next;
-    temp.next = null;
-    this.length--;
+    let temp = this.head;
 
-    if (this.length === 0) {
+    if (this.length === 1) {
+      this.head = null;
       this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
     }
+
+    this.length--;
 
     return temp;
   }
@@ -93,8 +97,15 @@ class LinkedList {
 
     let temp = this.head;
 
-    for (let i = 0; i < index; i++) {
-      temp = temp.next;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = this.tail;
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
     }
 
     return temp;
@@ -115,20 +126,22 @@ class LinkedList {
     if (index === 0) {
       return this.unshift(value);
     }
-
     if (index === this.length) {
       return this.push(value);
     }
-
     if (index < 0 || index > this.length) {
       return false;
     }
 
-    let newNode = new Node(value);
-    let temp = this.get(index - 1);
+    const newNode = new Node(value);
+    let before = this.get(index - 1);
+    let after = before.next;
 
-    newNode.next = temp.next;
-    temp.next = newNode;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+
     this.length++;
 
     return true;
@@ -136,43 +149,29 @@ class LinkedList {
 
   remove(index) {
     if (index === 0) {
-      return this.shift();
+      return this.shift(index);
     }
 
     if (index === this.length - 1) {
-      return this.pop();
+      return this.pop(index);
     }
 
     if (index < 0 || index >= this.length) {
-      return false;
+      return undefined;
     }
 
-    let before = this.get(index - 1);
-    let temp = before.next;
+    let temp = this.get(index);
 
-    before.next = temp.next;
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+
+    temp.prev = null;
     temp.next = null;
 
     this.length--;
 
     return temp;
   }
-
-  reverse() {
-    let temp = this.head;
-    this.head = this.tail;
-    this.tail = temp;
-    let next = temp.next;
-    let prev = null;
-
-    for (let i = 0; i < this.length; i++) {
-      next = temp.next;
-      temp.next = prev;
-      prev = temp;
-      temp = next;
-    }
-
-    return this;
-  }
 }
 
+let myDoublyLikedList = new DoublyLinkedList(3);
